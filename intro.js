a081b7a96b39af65526684386d133fb4cac70276
faheemdeveloper <fm972220@gmail.com>
2024-08-27 09:52:11 -0700

    mode change

--- intro.js
+++ intro.js
@@ -1,18 +1,23 @@
-let studentNames = [
-  "Faheem",
-  "wasid",
-  "shehzad",
-  "younes",
-  "adil",
-  "basit",
-  "shahzaib",
-];
+// Mode Changing
 
-let studentNames2 = [
-    "Azhar",
-    "imran"
-]
+let box = document.querySelector("div");
 
-let res = studentNames.concat(studentNames2);
+let newbtn = document.createElement("button");
+let color = "green"
 
-console.log(res);
+newbtn.innerText = "Shop Now";
+box.append(newbtn);
+newbtn.style.padding = "0.8rem 2rem";
+newbtn.style.fontSize = "0.8rem";
+newbtn.style.borderRadius = "1rem";
+
+box.addEventListener("click", () => {
+    if(color === "green") {
+        color = "blue";
+        document.querySelector("div").style.backgroundColor = "skyblue";
+        document.querySelector("div").style.Color = "white";
+    } else {
+        color = "green";
+        document.querySelector("div").style.backgroundColor = "green";
+    }
+});
