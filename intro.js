0a264282ae1be517d4a950f0f37269430f1191ed
faheemdeveloper <fm972220@gmail.com>
2024-09-10 21:42:13 -0700

    map method

--- intro.js
+++ intro.js
@@ -1,23 +1,7 @@
-// Mode Changing
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
-let box = document.querySelector("div");
-
-let newbtn = document.createElement("button");
-let color = "green"
-
-newbtn.innerText = "Shop Now";
-box.append(newbtn);
-newbtn.style.padding = "0.8rem 2rem";
-newbtn.style.fontSize = "0.8rem";
-newbtn.style.borderRadius = "1rem";
-
-box.addEventListener("click", () => {
-    if(color === "green") {
-        color = "blue";
-        document.querySelector("div").style.backgroundColor = "skyblue";
-        document.querySelector("div").style.Color = "white";
-    } else {
-        color = "green";
-        document.querySelector("div").style.backgroundColor = "green";
-    }
+const result = numbers.map((currElem) => {
+    return currElem  * 5;
 });
+
+
