b6731efa56f942a4006e3911543beda931c03820
faheemdeveloper <fm972220@gmail.com>
2024-09-10 22:53:10 -0700

    map

--- intro.js
+++ intro.js
@@ -1,7 +1,11 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// The Square of Even numbers in map mathod
 
-const result = numbers.map((currElem) => {
-    return currElem  * 5;
-});
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+
+let result = numbers.map((currElem) => {
+    if(currElem % 2 === 0) {
+        return currElem * currElem;
+    }
+}).filter((currElem) => currElem !== undefined);
 
