fccb49904d10c127a1e6dd70e81389a7c4592b79
faheemdeveloper <fm972220@gmail.com>
2024-09-25 22:25:11 -0700

    updated

--- intro.js
+++ intro.js
@@ -1,38 +1,28 @@
-let product = [
-  {name: "laptop", price: 1200},
-  {name: "phone", price: 150},
-  {name: "tablate", price: 500},
-  {name: "SmartWatch", price: 300},
-]
+// Local Storage
 
-let updateProduct = product.filter((currElem) => {
-  return currElem.price <= 500;
-});
+let studentProfile = {
+  fullName: "Faheem",
+  age: 22,
+  marks: 502,
+  Grade: "D",
+  learning: "JavaScript",
+};
 
-console.log(updateProduct);
 
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let addList = () => {
+//     localStorage.setItem("StudentProfile", JSON.stringify(studentProfile));
+// }
 
-let result = numbers.map((currElem) => {
-  if(currElem % 2 === 0) {
-    return currElem * currElem
-  }
-}).filter((currElem) => currElem !== undefined);
 
-console.log(result)
+// document.querySelector(".btn").addEventListener("click", () => {
+//     addList()
+// });
 
-let numbers = [100, 200, 300, 400, 500, 600];
+let getlist = () => {
+    let getObj = JSON.parse(localStorage.getItem("StudentProfile"));
+    console.log(getObj)
+};
 
-let totalProduct = numbers.reduce((accum, currElem) => {
-  return accum + currElem;
-}, 0);
-
-console.log(totalProduct);
-
-let students = ["Faheem", "Wasid","shehzad", "shahzaib", "adil", "basit",];
-
-let result = students.map((currEleem) => {
-  return currEleem.toUpperCase()
-});
-
-console.log(result);
+document.querySelector(".btn").addEventListener("click", () => {
+    getlist()
+})
