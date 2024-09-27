9b1fa9b5e62a2826faa1665701008eb1c6d8f8f3
faheemdeveloper <fm972220@gmail.com>
2024-09-26 22:31:59 -0700

    date time Updated

--- intro.js
+++ intro.js
@@ -1,23 +1,66 @@
 // Date and Time
-
-// let date = new Date().getTime();
-// console.log(date)
-
-// let milisec = 1727413384987;
-
-// let newDAte = new Date(milisec);
-
-// console.log(newDAte);
-
-// let newDate = new Date();
-// console.log(newDate);
-// let year = newDate.getFullYear();
-
-// console.log(year)
-
+/*
+let date = new Date().getTime();
+console.log(date)
+let milisec = 1727413384987;
+let newDAte = new Date(milisec);
+console.log(newDAte);
+*/
+// Get Date Methods
+/*
+let newDate = new Date();
+console.log(newDate);
+let year = newDate.getFullYear();
+console.log(year)
 let date = new Date();
 console.log(date)
 let month = date.getMonth();
 let day = date.getDate();
 console.log(month)
 console.log(day)
+*/
+
+// Get Time Methods
+/*
+let date = new Date();
+console.log(date)
+let hours = date.getHours();
+let minuts = date.getMinutes();
+let seconds = date.getSeconds();
+console.log(hours);
+console.log(minuts);
+console.log(seconds);
+*/
+
+// Set date Methods
+/*
+let date = new Date();
+console.log(date)
+let updatedYear = date.setFullYear(2028);
+console.log(updatedYear);
+const newDate = new Date(updatedYear);
+console.log(newDate);
+let newMonth = date.setMonth(2);
+console.log(newMonth);
+let toMonth = new Date(newMonth);
+console.log(toMonth);
+let setDate = date.setDate(20);
+console.log(setDate);
+let toDate = new Date(setDate);
+console.log(toDate);
+*/
+
+// Set Time Methods
+/*
+let date = new Date();
+console.log(date);
+let setHour = date.setHours(4);
+console.log(setHour);
+let changeHour = new Date(setHour);
+console.log(changeHour)
+let setMinuts = date.setMinutes(20);
+console.log(setMinuts);
+let changeMinuts = new Date(setMinuts);
+console.log(changeMinuts)
+*/
+
