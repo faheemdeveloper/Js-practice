fe147043b891bcd670a6496f1eea2478ed8d8ad1
faheemdeveloper <fm972220@gmail.com>
2024-10-01 04:09:03 -0700

    em 2017

--- intro.js
+++ intro.js
@@ -1,75 +1,157 @@
-const name = "Faheem";
-const age = 22;
-
-const person = {name : name, age : age};
-console.log(person)
-
-const user = {
-  name: "Faheem",
-  age: 22,
-};
-
-const myName = user;
-const  {name, age} = user;
-
-console.log(`my name is ${name} and my age is  ${age}`);
-
-const user = {
+// Ecma Script 2017
+/*
+let student = {
   myName: "Faheem",
   age: 22,
 };
 
-let numbers = [...number1, ...number2];
-console.log(numbers);
+console.log(student);
 
-let student = ["Faheem", "wasdi", "shehzad"];
-student.push(...["adil", "sohail"]);
+let myName = "Faheem";
+let age = 22;
+
+Excesing Traditinol Way
+// let profile = {myName : myName, age : age};
+New Way
+let profile = {myName, age};
+console.log(profile);
+
+const myName = "Faheem";
+const age = 22;
+
+// let student = {myName : myName, age : age};
+let student = {myName, age}
 
 console.log(student);
 
-let country = "Pakistan";
-console.log(country.split(""));
+Accesing Array Element
 
+let arr = [10, 20, 30, 40, 50];
+// // Traditinol Way
+// // let first = arr[3];
+// // New Way
+
+// [first, second, third, four, five] = arr
+// console.log(five)
+
+[first, Second, third, four, five] = arr;
+console.log(five)
+
+let arr = [10, 20, 30, 40, 50];
+// [a, b, c, d, f] = arr;
+// console.log(f)
+// ignoring Element
+[, , , , last] = arr;
+
+console.log(last)
+
+Swaping Properties
+let a = 20;
+let b = 50;
+
+con
+sole.log("a =", a,"b =",  b);
+[a, b] = [b, a];
+
+console.log("a =",a, "b =", b);
+
+let a = 30;
+let b = 70;
+console.log("a =",a, "b =", b);
+[a, b] = [b, a];
+console.log()
+console.log("a =",a, "b =", b);
+
+Destruturing Properties with Object
+let obj = {
+    myName : "Faheem",
+    age : 22,
+    marks : 502,
+};
+
+const {myName, age, marks} = obj;
+console.log(marks);
+const {myName, age, marks} = obj;
+console.log(age)
+
+let obj = {
+  myName: "Faheem",
+  age: 22,
+  marks: 502,
+  Grade: "A",
+};
+
+// let newOBj = obj.marks;
+let {myName, age, marks, Grade} = obj;
+console.log(Grade)
+
+// Renaming Properties
+let obj = {
+    myName : "Faheem",
+    age : 22
+};
+
+// {myName : fullName, age : myAge} = obj;
+let {myName, age} = obj
+
+console.log(age)
+
+let arr = ["Faheem", "wasid", "adil", "sohail"];
+let newArr = [...arr];
+
+console.log(newArr);
+
+let students = ["Faheem", "sohail", "shehzad", "wasid"];
+let newArr = [...students];
+console.log(newArr);
+
+let number1 = [1, 2, 3, 4];
+let number2 = [4, 5, 6, 7];
+let numbers = [...number2 ,...number1,];
+console.log(numbers);
+
+
+adding Element in Array with spread Operator
+let student = ["Faheem", "wasid", "shehzad", "shahzaib"];
+student.push(...["sohail", "adil", "qashif"])
+
+console.log(student);
+
+const country = "Pakistan";
+Traditional Way
+console.log(country.split(""))
+New Way
 console.log([...country]);
+*/
+// The sum of multiple number
+// Traditional Way
+/*
+let sum = (a, b, c, d, e) => {
+    return a + b + c + d + e;
+};
 
-let myName = "Faheem";
-let withSpace = myName.padStart(18, "-");
-let endSpace = myName.padEnd(15);
-console.log(withSpace);
-console.log(endSpace.length);
-console.log(myName.length);
+console.log(sum(1, 2, 3, 4, 5));
+*/
 
-const obj1 = {a : 10, b: 20, c: 50};
-const obj2 = {c : 30, d: 50};
+// New Way with Spread Operator
+/*
+let sum = (...hello) => {
+    return hello.reduce((occum, currElem) => (occum + currElem), 0);
+};
 
-let newObj = {...obj1 ,...obj1,};
-console.log(newObj);
+console.log(sum(3, 4,));
+let sum = (...numbers) => {
+    return numbers.reduce((occum, currElem) => {
+        return occum + currElem;
+    });
+};
+console.log(sum(3,4 ));
 
-let person = { naem: "Faheem", age: 22 };
-let enteries = Object.entries(person);
-console.log(enteries);
+let sum = function (...number) {
+    return number.reduce((occum, currElem) => {
+        return occum + currElem;
+    });
+};
+console.log(sum(34, 3,))
+*/
 
-let newPerosn = Object.fromEntries(enteries);
-console.log(newPerosn);
-
-const mxNumber = Number.MAX_SAFE_INTEGER;
-console.log(mxNumber);
-// console.log(mxNumber);
-
-let favNum = undefined;
-userFavNum = favNum ?? 10;
-console.log(userFavNum)
-const myName = user.myName;
-const age = user.age;
-
-console.log(myName, age)
-
-let students = ["Faheem", "wasid", "shehzad", "shahzaib", "adil"];
-
-let NewStudents = [...students];
-
-console.log(students)
-console.log(NewStudents)
-
-let number1 = [1, 2, 3];
-let number2 = [4, 5, 6];
