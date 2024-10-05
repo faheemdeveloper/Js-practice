d155e91ede7b05790132befc1bb311b3f81791c1
faheemdeveloper <fm972220@gmail.com>
2024-10-05 00:22:00 -0700

    promises

--- intro.js
+++ intro.js
@@ -147,28 +147,133 @@
 
 // console.log(newFunction());
 
-let promise1 = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve("First");
-  });
-});
+// let promise1 = new Promise((resolve) => {
+//   setTimeout(() => {
+//     resolve("First");
+//   });
+// });
 
-let promise2 = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve("Second");
-  }, 3000);
-});
+// let promise2 = new Promise((resolve) => {
+//   setTimeout(() => {
+//     resolve("Second");
+//   }, 3000);
+// });
 
-let promise3 = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve("Second");
-  }, 3000);
-});
+// let promise3 = new Promise((resolve) => {
+//   setTimeout(() => {
+//     resolve("Second");
+//   }, 3000);
+// });
 
-Promise.all([promise1, promise2, promise3])
-  .then((res) => {
-    console.log(res);
-  }, 3000)
-  .catch((erorr) => {
-    console.log(erorr);
-  });
+// Promise.all([promise1, promise2, promise3])
+//   .then((res) => {
+//     console.log(res);
+//   }, 3000)
+//   .catch((erorr) => {
+//     console.log(erorr);
+//   });
+
+// CallBack function
+// function greet(name, callback) {
+//   console.log("Hello ", name);
+//   callback();
+// }
+
+// function saygood() {
+//   console.log("Goood bye");
+// }
+
+// greet("faheem", saygood());
+
+// Higher Order Function
+
+// function input(name, greetuser) {
+//   console.log(`Reservid Input: ${name}`);
+// }
+
+// function greetuser(name) {
+//   console.log(`Hello ${name}`);
+// }
+// input(`Faheem ${greetuser()}`);
+
+// function mathOperation(a, b, operation) {
+//   return operation(a, b);
+// }
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// console.log(mathOperation(4, 5, add))
+
+// function greet(name, callback) {
+//   console.log("hello ", name);
+//   callback();
+// }
+
+// function add() {
+//   console.log("goodbye");
+// }
+
+// greet("faheem", add);
+
+// function greet(name, hello) {
+//   console.log("hello ", name)
+//   hello()
+// }
+
+// function add() {
+//   console.log("hello gays")
+// }
+
+// greet("faheem", add)
+
+// async function myFunction() {
+//   console.log("hello Faheem");
+// }
+
+// myFunction().then((res) => {
+//   return res
+// })
+
+// function resolveAllAfter2Second() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve("resolve success");
+//     }, 3000);
+//   });
+// }
+
+// async function asyncCall() {
+//   console.log("caling...");
+//   let result = await resolveAllAfter2Second();
+//   console.log(result)
+// }
+
+// asyncCall();
+
+// Using Promises
+// function fechData() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve("Data Fetched");
+//     }, 3000);
+//   });
+// }
+
+// fechData().then((res) => {
+//   console.log(res)
+// }).catch((error) => {
+//   console.log(error)
+// })
+
+// Using Async await
+
+async function getData() {
+  
+try{
+  let result = await fetchData();
+}catch(error) {
+  console.log(error)
+}
+}
