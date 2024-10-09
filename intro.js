f1c0552a1c606dea4d8aeed035a22ca627dcda0a
faheemdeveloper <fm972220@gmail.com>
2024-10-09 04:07:57 -0700

    js Update

--- intro.js
+++ intro.js
@@ -238,7 +238,7 @@ console.log(sortedValue) */
 // console.log(numbers.reverse())
 
 // Strings
-let str = "Hello JavaScript welcome to world best Javascript";
+// let str = "Hello JavaScript welcome to world best Javascript";
 
 // let checkIndexOfWord = str.indexOf("J");
 // let lastIndexOf = str.lastIndexOf("J");
@@ -266,10 +266,252 @@ let str = "Hello JavaScript welcome to world best Javascript";
 //   console.log(String.fromCharCode(char))
 // }
 
-// console.log(Math.round(4.5)); 
-// console.log(Math.floor(4.9));
-// console.log(Math.ceil(4.1));
-// console.log(Math.abs(4.5));
-// console.log(Math.trunc(4.9));
-// console.log(Math.sqrt(7).toFixed(1));
+// console.log(Math.round(40.5));
+// console.log(Math.floor(40.5));
+// console.log(Math.ceil(40.5));
+// console.log(Math.trunc(40.9));
+// console.log(Math.abs(40.9));
+// console.log(Math.sqrt(40).toFixed(1));
 
+// localStorage.removeItem("new key");
+
+/*
+let obj = {
+  myName: "Faheem",
+  age: 22,
+  marks: 502,
+  Grade: "D",
+  language : "JavaScript",
+  bloodGroup : "A+"
+};
+
+let setData = () => {
+  localStorage.setItem("newStudent", JSON.stringify(obj));
+}
+
+document.querySelector(".btn").addEventListener("click", function () {
+  setData()
+});
+
+const getData = () => {
+  let retrive = JSON.parse(localStorage.removeItem("newProfile"));
+  console.log(retrive)
+}
+
+document.querySelector(".btn").addEventListener("click", () => {
+  getData()
+})
+*/
+
+// Date and Time
+/*
+let date = new Date().getTime();
+
+console.log(date);
+let date1 = new Date(1728449733684);
+console.log(date1);
+let time = new Date().getDate();
+
+console.log(time);
+
+let date = new Date(8);
+console.log(date);
+
+let time = new Date().getTime();
+console.log(time);
+
+let date = new Date().setFullYear(2025)
+console.log(date)
+const year = new Date(1759990994542);
+console.log(year);
+
+const time = new Date().getMinutes();
+console.log(time)
+
+const time = new Date().toDateString();
+console.log(time);
+*/
+/*
+let myfunction = (x) => {
+ return setTimeout(() => {
+  console.log("Hello JavaScript", x)
+}, 4000)
+}
+
+console.log(myfunction(34));
+
+Objects
+let myName = "StudentName"
+const students = {
+  [myName] : "Faheem",
+  sAge: 22,
+  marks: 502,
+  grade: "D",
+};
+
+console.log(`The ${myName} is ${students[myName]} and ${"sAge"} is ${students.sAge}`);
+
+let target = {a : 10, b : 20};
+let source = {b : 40, c : 50};
+let marge = Object.assign({}, source, target);
+console.log(marge);
+
+let obj1 = {
+  myName: "Faheem",
+  age: 22,
+  marks: 502,
+  grade: "D",
+};
+
+let entres = Object.entries(obj1);
+console.log(entres);
+
+let myName = "Faheem";
+let age = 22;
+let marks = 502;
+let Grade = "A";
+
+// let obj = { myName, age, marks, Grade };
+let obj = { myName : myName, age : age, marks : marks, Grade : Grade };
+console.log(obj);
+
+let arr = ["Faheem", "wasid", "shehzad"];
+
+[, , third] = arr;
+console.log(third);
+let a = 10;
+let b = 30;
+[a, b] = [b, a];
+console.log("a =", a, "b =", b);
+
+let obj1 = {
+  myName: "Faheem",
+  age: 22,
+  marks: 502,
+  grade: "D",
+};
+
+let { myName, age, marks, grade } = obj1;
+console.log(myName, age, marks, grade);
+
+let student = {
+  myName: "Faheem",
+  age: 22,
+  marks: 502,
+};
+
+const {myName, age, marks} = student;
+console.log(marks);
+*/
+/*
+let arr = ["Faheem", "wasid", "shehzad", "adil"];
+let arr2 = ["basit", "sohail"];
+let fullArr = [...arr, ...arr2];
+// let newArr = [...arr];
+console.log(fullArr);
+
+let arr = ["Faheem", "wasid", "shehzad", "adil"];
+arr.push(...["basit", "sohail"]);
+console.log(arr);
+
+let func1 = (a, b, c, d, e) => {
+  console.log(a + b + c + d + e);
+};
+
+func1(1, 2, 3, 4, 5);
+
+let func2 = (...number) => {
+  return number.reduce((occum, currElem) => {
+    return occum + currElem;
+  });
+};
+
+console.log(func2(4, 2, 4, 2, 2,5, 3, 1, 7));
+
+const sym = Symbol("this is symbol data type");
+console.log(sym, typeof sym);
+console.log(sym.description)
+let btn = document.querySelector(".btn");
+
+let myFunction = () => {
+  setTimeout(() => {
+    btn.innerHTML = "Hello Button"
+    setTimeout(() => {
+    btn.innerHTML = "Call Back Button"
+
+      setTimeout(() => {
+        console.log("Third");
+        setTimeout(() => {
+          console.log("Fourth");
+          setTimeout(() => {
+            console.log("Fivth");
+            setTimeout(() => {
+              console.log("Six");
+              setTimeout(() => {
+                console.log("Siven");
+                setTimeout(() => {
+                  console.log("High");
+                  setTimeout(() => {
+                    console.log("Nine");
+                    setTimeout(() => {
+                      console.log("Ten");
+                    }, 2000);
+                  }, 2000);
+                }, 2000);
+              }, 2000);
+            }, 2000);
+          }, 2000);
+        }, 2000);
+      }, 2000);
+    }, 2000);
+  }, 2000);
+};
+
+myFunction() */
+
+// let promise = new Promise((resolve, reject) => {
+//   let success = true;
+//   if (success) {
+//     setTimeout(() => {
+//       resolve("Promise is fulfilled successful");
+//     }, 4000);
+//   } else {
+//     setTimeout(() => {
+//       reject("Promise is rejected");
+//     }, 4000);
+//   }
+// });
+
+// promise
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+let function22 = () => {
+  return new Promise((resolve, reject) => {
+    let success = true;
+    if(success) {
+      setTimeout(() => {
+        resolve("promise fulfilled");
+      }, 3000)
+    } else {
+      setTimeout(() => {
+        reject ("promise is rejected")
+      })
+    }
+  });
+};
+
+async function sum() {
+  try {
+    let result = await function22();
+    console.log(result, calling)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+sum()
