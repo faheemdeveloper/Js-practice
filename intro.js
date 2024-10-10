cce9cdcba8e421da3166c7fa9373a1379bef7992
faheemdeveloper <fm972220@gmail.com>
2024-10-10 02:37:57 -0700

    Operators

--- intro.js
+++ intro.js
@@ -45,74 +45,94 @@
 // console.log(toBol, typeof toBol);
 
 // ********************* Operations  *********************
-// let value = 3;
-// let negVal = -value;
-// // console.log(negVal);
+/*
+let value = 3;
+let negVal = -value;
+// console.log(negVal);
 
-// // console.log(+"Faheem")
+// console.log(+"Faheem")
 
-// let num1, num2, num3;
-// num1, num2, (num3 = 2 + 2);
-// console.log(num2);
+let num1, num2, num3;
+num1, num2, (num3 = 2 + 2);
+console.log(num2);
 
-// let myName = "Faheem";
-// let myAge = 22;
-// let marks = 502;
-// let Grade = "D";
+let myName = "Faheem";
+let myAge = 22;
+let marks = 502;
+let Grade = "D";
 
-// console.table([myName, myAge, marks, Grade]);
+console.table([myName, myAge, marks, Grade]);
 
-// console.log(3 + 5 + "8");
-// console.log("3" + 5 + 8);
-// console.log(true + "4" + 3);
-// console.log("4" + 3 + false);
-// console.log(true - "3");
-// console.log(false - "4" + 4);
-// let value = 4;
-// let negValue = +value;
-// console.log(negValue)
+console.log(3 + 5 + "8");
+console.log("3" + 5 + 8);
+console.log(true + "4" + 3);
+console.log("4" + 3 + false);
+console.log(true - "3");
+console.log(false - "4" + 4);
+let value = 4;
+let negValue = +value;
+console.log(negValue)
 
-// var age = 22;
-// var age = 22 + 2;
-// console.log(age);
+var age = 22;
+var age = 22 + 2;
+console.log(age);
 
-// let age = 22;
-// age + 3;
-// console.log(age)
+let age = 22;
+age + 3;
+console.log(age)
 
-// const age = 22;
-// age = 22 + 3;
-// console.log(age);
+const age = 22;
+age = 22 + 3;
+console.log(age);
 
-// let a = 5;
-// let b = 3;
-// a += 4;
-// console.log(a + b);
+let a = 5;
+let b = 3;
+a += 4;
+console.log(a + b);
 
-// let myName = "M_";
-// myName += "Faheem";
-// console.log(myName);
-
+let myName = "M_";
+myName += "Faheem";
+console.log(myName);
+*/
 // *********************** Arithematic Operators ***********************
-// let a = 3;
-// let b = 8;
-// [a, b] = [b, a]
-// let devide = a / b;
-// console.log("a =", a, "b =", b);
-// console.log("a + b =", a + b);
-// console.log("a - b =", a - b);
-// console.log("a * b =", a * b);
-// console.log("a / b =", a / b);
-// console.log("a % b =", a % b);
-// console.log(devide.toFixed(1))
-// console.log("a ** b =", a ** b);
+/*
+let a = 3;
+let b = 8;
+[a, b] = [b, a]
+let devide = a / b;
+console.log("a =", a, "b =", b);
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+console.log(devide.toFixed(1))
+console.log("a ** b =", a ** b);
+*/
+// *********************** Ternery Operators ***********************
+/*
+let a = 8;
+let b = 3;
+console.log("a =", a, "b =", b);
 
-// let a = 8;
-// let b = 3;
-// console.log("a =", a, "b =", b);
+let age = 19;
+let license = true;
+let result = age >= 18 && license;
+console.log(result);
+*/
+// *********************** Comparision Operators ***********************
+/*
+let a = 8;
+let b = 9;
+console.log("a =", a, "b =", b);
 
-// let age = 19;
-// let license = true;
+console.log("a == b", a == b);
+console.log("a === b", a === b);
+console.log("a != b", a != b);
+console.log("a !== b", a !== b);
+console.log("a > b", a > b);
+console.log("a < b", a < b);
+console.log("a >= b", a >= b);
+console.log("a <= b", a <= b);
+*/
 
-// let result = age >= 18 && license;
-// console.log(result);
