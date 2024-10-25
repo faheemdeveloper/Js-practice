9b3be9402b466855ab9336f5d7b976a3ea4dd66b
faheemdeveloper <fm972220@gmail.com>
2024-10-24 20:31:52 -0700

    promises and async await are updated

--- intro.js
+++ intro.js
@@ -1463,7 +1463,7 @@ function toggleMenu() {
 //   clearInterval(starbtn)
 // })
 
-let btn = document.querySelector('#animateBtn');
+// let btn = document.querySelector('#animateBtn');
 
 // btn.addEventListener("mouseover", function() {
 //   btn.style.backgroundColor = "orange"
@@ -1518,3 +1518,291 @@ let btn = document.querySelector('#animateBtn');
 // dive.appendChild(textNode)
 // container.appendChild(dive)\
 
+// let start = document.querySelector("#start")
+// let stop = document.querySelector("#stop")
+// let dive = document.querySelector(".new-div")
+
+// let startFunc = () => {
+//   dive.innerHTML = Math.floor(Math.random() * 10 + 1)
+// }
+
+// let changing;
+
+// start.addEventListener("click", () => {
+//   changing = setInterval(startFunc, 1000)
+// })
+
+// stop.addEventListener("click", () => {
+//   clearInterval(changing)
+// })
+
+// *********************************** Promises and async await ***********************************
+
+// const promiseOne = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("Async tast is complated");
+//     resolve()
+//   }, 3000)
+// })
+
+// promiseOne.then(() => {
+//   console.log("Promise is consumed")
+// })
+
+// new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     console.log("Async task 2 is complated")
+//     resolve()
+//   })
+// }).then(function () {
+//   console.log("Async 2 is consumed")
+// })
+
+// const promiseThree = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     resolve({ username: "Faheem", email: "faheem@gmail.com" })
+//   }, 3000)
+// })
+
+// promiseThree.then((user) => {
+//   console.log(user)
+// })
+
+// const promiseFour = new Promise(function(resolve, reject) {
+//   setTimeout(() => {
+//     let error = false;
+//     if(error) {
+//       resolve(["Faheem", "wasid", "shehzad", "sohail"])
+//     } else {
+//       reject ("ERROR: sum error accurd")
+//     }
+//   }, 2000);
+// })
+
+
+// promiseFour.then((user) => {
+//   console.log(user);
+//   return user[2]
+// }).then((username) => {
+//   console.log(username)
+// }).catch((error) => {
+//   console.log(error)
+// }).finally(() => {
+//   console.log("finnaly all done")
+// })
+
+// const promiseFive = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     let error = true;
+//     if (error) {
+//       resolve({ username: "Faheem", password: "1234" });
+//     } else {
+//       reject("ERROR: Js went wrong")
+//     }
+//   }, 1000)
+// })
+
+// async function promiseFiveConsum() {
+//   try {
+//     const response = await promiseFive;
+//     console.log(response)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// promiseFiveConsum()
+
+// async function getAllUsers() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = await response.json();
+//     console.log(data)
+
+//   } catch (error) {
+//     console.log( error)
+//   }
+// }
+
+// getAllUsers()
+
+
+// let promiseOne = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     let succ = true;
+//     if (succ) {
+//       resolve(["Faheem", "wasid", "shehzad", "said mehmood", "sohail", "shehzad", "shahzeb"])
+//     } else {
+//       reject("ERROR: promise is rejected")
+//     }
+//   }, 2000)
+// })
+
+// let consumPromise = async () => {
+//   try {
+//     let response = await promiseOne;
+//     console.log(response)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// consumPromise()
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//   // .then(response => response.json())  // Parsing the response data into JSON
+//   .then((response) => {
+//     return response.json()
+//   })
+//   .then(data => {
+//     console.log(data);  // Handling the data
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);  // Handling errors
+//   });
+
+
+// async function getAllUsers() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = await response.json();
+//     console.log(data)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// getAllUsers()
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((res) => {
+//     return res.json()
+//   })
+//   .then((data) => {
+//     console.log(data)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
+
+
+// const getAll = async () => {
+//   try {
+//     let response = await fetch("https://jsonplaceholder.typicode.com/users")
+//     let data = await response.json()
+//     console.log(data)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// getAll()
+
+// let promiseOne = new Promise((resolve, reject) => {
+//   let error = false
+//   if(error) {
+//     setTimeout(() => {
+//       console.log("Promise is succes")
+//       resolve()
+//     }, 3000)
+//   } else {
+//     setTimeout(() => {
+//       console.log("Error accurd")
+//     }, 3000)
+//   }
+
+// })
+
+// promiseOne
+//   .then((response) => {
+//     console.log(response)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
+
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((res) => {
+//     return res.json()
+//   })
+//   .then((data) => {
+//     console.log(data)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
+
+// let getAllUsers = async () => {
+//   try {
+//     let response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     let data = await response.json()
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+
+// let PromiseHere = new Promise((reslove, reject) => {
+//   setTimeout(() => {
+//     reject({username : "Faheem", email : "info@gmail.com"});
+//   }, 3000)
+// })
+
+// PromiseHere.then((response) => {
+//   console.log(response);
+//   return response.username
+// }).then((user) => {
+//   console.log(user)
+// }).catch((error) => {
+//   console.log(error)
+// })
+
+// let PromiseOne = new Promise(function (reslove, reject) {
+//   setTimeout(() => {
+//     reslove(["Faheem", "shehzad", "sohail", "wasid"])
+//   }, 3000)
+// })
+//   .then((respon) => {
+//     console.log(respon)
+//     return respon[2]
+//   })
+//   .then((user) => {
+//     console.log(user)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
+
+// let getting = async () => {
+//   try {
+//     let response = await PromiseOne
+//     console.log(response)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+
+// let fetchingData = async () => {
+//   try {
+//     let hell = await fetch("https://jsonplaceholder.typicode.com/users");
+//     let data = await hell.json()
+//     console.log(data)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// fetchingData()
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((Response) => {
+    return Response.json()
+  })
+  .then((getting) => {
+    console.log(getting)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
